02bc64d68768e8e588585c785004f6131b40d9db
Paul D <deck0017@algonquinlive.com>
2026-08-04 21:16:50 -0400

    Updates app.vue for contact info

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="app">
+    <header class="app-header">
+      <h1>Contact Book</h1>
+      <nav>
+        <RouterLink to="/">All Contacts</RouterLink>
+        <RouterLink to="/contacts/new">Add Contact</RouterLink>
+      </nav>
+    </header>
+
+    <main class="app-main">
+      <RouterView />
+    </main>
+  </div>
 </template>
+
+<script setup>
+</script>
